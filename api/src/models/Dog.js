973021a6690a2472f8d6a0a0fc375bbcde3d8568
Yairys <yayi.arist.hz@gmail.com>
2022-07-16 21:06:22 -0300

    first commit

---
+++ api/src/models/Dog.js
@@ -0,0 +1,60 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("Dog", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    height_min:{
+      type:DataTypes.STRING,
+      allowNull: false
+    },
+    height_max:{
+      type:DataTypes.STRING,
+      allowNull: false
+    },
+    weight_min:{
+      type:DataTypes.STRING,
+      allowNull: false
+    },
+    weight_max:{
+      type:DataTypes.STRING,
+      allowNull: false
+    },
+    life_time_min:{
+      type:DataTypes.STRING,
+      allowNull: false
+    },
+    life_time_max:{
+      type:DataTypes.STRING,
+      allowNull: false
+    },
+    createInDb:{
+      type:DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    },
+    img: {
+      type:DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "https://www.anipedia.net/imagenes/nombres-de-perros-800x375.jpg"
+     },
+    
+},
+  {
+    timestamps:false,
+    // timestamps: true,
+    // createdAt: false,
+    // updatedAt: "Actualizacion",
+  }
+  );
+};
