03e59e5f43d414e476eff335575e7fd2162e8f4f
Yairys <yayi.arist.hz@gmail.com>
2022-07-28 03:48:27 -0300

    falta css...

--- client/temporal.js
+++ client/temporal.js
@@ -2,6 +2,6 @@ async function handleSubmit(e){
   e.preventDefault()
   setErrors(validate(input))
   await axios.post("http://localhost:3001/create", input)
-  alert('perro creado')
+  alert('perrito creado')
    
 }
